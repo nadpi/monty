5ca53440496945165ac839e723e651f9dc1d5bdf
MinnaMohammed <barakatminna1@gmail.com>
2023-08-25 17:45:38 -0700

    second trial

--- main.c
+++ main.c
@@ -22,11 +22,9 @@ int main(int argc, char *argv[])
 	{
 	counter++;
 	oper = strtok(buffer, " \n\t");
-	if (oper == NULL || strlen(oper) == 0) {
+	if (oper == NULL || strlen(oper) == 0 || strstr(oper, "#")) {
         continue;
 	}
-	/*if (oper[0] == '#')
-		return (0);*/
 	omk1.arg = strtok(NULL, " \n\t");
 	if (oper)
 	{
