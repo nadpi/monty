58008c7032674fbe1aba7027f92700c7c0b70c72
Nada <nadawaled87@gmail.com>
2024-03-27 16:37:20 -0400

    [Mod Function]

---
+++ mod.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+/**
+ * fn_mod - computes the rest of the division of the top 2 elements
+ * @top: top
+ * @line: line
+ * Return: nothing
+ */
+void fn_mod(stack_t **top, unsigned int line)
+{
+	stack_t *curr;
+	int result = 0;
+
+	curr = *top;
+
+	if (!*top || (*top)->prev == NULL)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line);
+		exit(EXIT_FAILURE);
+	}
+	else if ((*top)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line);
+		exit(EXIT_FAILURE);
+	}
+
+	result = (curr->prev->n) % (curr->n);
+	curr->prev->n = result;
+	*top = curr->prev;
+	free(curr);
+}
